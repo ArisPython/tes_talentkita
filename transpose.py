49c0ee2ce2a7ddeb0347d1f3c93945e2b5a23610
aris <winandiaris@gmail.com>
2022-08-14 20:30:50 +0700

    md

--- transpose.py
+++ transpose.py
@@ -1,18 +1,19 @@
+
+# Transpose matriks adalah matriks baru yang diperoleh dengan cara menukar elemen-elemen baris menjadi elemen kolom atau sebaliknya
 # Fungsi Transpose
 def transpose(l1, l2):
  
-    # iterate over list l1 to the length of an item
+    # looping l1 sampai sepanjang item
     for i in range(len(l1[0])):
         # print(i)
         row =[]
         for item in l1:
-            # appending to new list with values and index positions
-            # i contains index position and item contains values
+            # Menambahkan nilai (item) dengan indexnya (i) kedalam list baru 
             row.append(item[i])
         l2.append(row)
     return l2
  
-# Driver code
+# Array 2d
 l1 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
 l2 = []
 print(transpose(l1, l2))
